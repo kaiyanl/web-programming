8713a0b2cbcfe7b3fdc3938ab83c98de381bbe02
DLavados <35505809+DLavados@users.noreply.github.com>
2019-04-10 16:50:10 -0700

    Update exhibit.js

--- Assignment2/source/exhibit.js
+++ Assignment2/source/exhibit.js
@@ -28,3 +28,60 @@ function disappear2(less, btn) {
 		mybtn.innerHTML = "Read more ->";
 	}
 }
+
+
+function dissapear3(less, img, btn) {
+	
+	let words = document.getElementByID(less);
+	let img = document.getElementsByClassName(img);
+	let mybtn = document.getElementById(btn);
+	
+	if (words.classList === "lessContent"){
+		words.classList.remove("lessContent");
+		words.classList.add("moreInitialContent");
+		img.classList.remove("lessContent");
+		img.classList.add("moreInitialContent");
+		mybtn.innerHTML = "Read less ->";
+    } else {
+		words.classList.remove("moreInitialContent");
+		words.classList.add("lessContent");
+		img.classList.remove("moreInitialContent");
+		img.classList.add("lessContent");
+		mybtn.innerHTML = "Read more ->";
+	}
+}
+
+
+/*
+function dissapear3(less, img, div, footer, btn) {
+	
+	let words = document.getElementByID(less);
+	let img = document.getElementsByClassName(img);
+	let div = document.getElementsByClassName(div);
+	let ftr = document.getElementsByClassName(footer);
+	let mybtn = document.getElementById(btn);
+	
+	if (words.classList === "lessContent"){
+		words.classList.remove("lessContent");
+		words.classList.add("moreInitialContent");
+		img.classList.remove("lessContent");
+		img.classList.add("moreInitialContent");
+		div.classList.remove("lessContent");
+		div.classList.add("moreFlexContent");
+		ftr.classList.remove("lessContent");
+		ftr.classList.add("moreInitialContent");
+		mybtn.innerHTML = "Read less ->";
+	} else {
+		words.classList.remove("moreInitialContent");
+		words.classList.add("lessContent");
+		img.classList.remove("moreInitialContent");
+		img.classList.add("lessContent");
+		div.classList.remove("moreFlexContent");
+		div.classList.add("lessContent");
+		ftr.classList.remove("moreInitialContent");
+		ftr.classList.add("lessContent");
+		mybtn.innerHTML = "Read more ->";
+	}
+	
+}
+*/
