3f777446236e1633bb727c8c66248c8d49ca63b3
Kaiyan Li <kkyli@ucdavis.edu>
2019-04-24 03:11:56 -0700

    add icon, update slide-up/down animation

--- Assignment3/Readme.txt
+++ Assignment3/Readme.txt
@@ -1,8 +1,10 @@
 To be solved:
 
-btn color font-size
-nothern color
-input background color padding-left?
+btn: color font-size
+nothern: color
+input: background color padding-left?
+
+function movedown: (async) animation first, lessContent second 
 
 solved:
 space at the bottom: body min-height: 100 vh
--- Assignment3/source/weather.js
+++ Assignment3/source/weather.js
@@ -1,13 +1,23 @@
+"strict mode"
+
 function moveUp(movingDivs, btn) {
 
 	let dive = document.getElementsByClassName(movingDivs);
     let mybtn = document.getElementById(btn);
+    
+
     
     if(dive[0].classList.contains(movingDivs)) {
         dive[0].style.animationName = "slideUp";
         mybtn.removeAttribute("onclick");
         mybtn.setAttribute("onclick", "moveDown('movingDivs', 'btn1')");
     }
+    
+    if (dive[0].classList.contains("lessContent")){
+		dive[0].classList.remove("lessContent");
+        dive[0].classList.add("moreContent");
+    }
+    
 }
 
 function moveDown(movingDivs, btn) {
@@ -20,4 +30,17 @@ function moveDown(movingDivs, btn) {
         mybtn.removeAttribute("onclick");
         mybtn.setAttribute("onclick", "moveUp('movingDivs', 'btn1')");
     }
+    //todo: need async 
+    if (dive[0].classList.contains("moreContent")){
+		dive[0].classList.remove("moreContent");
+        dive[0].classList.add("lessContent");
+    }
+}
+
+function submit() {
+
+    let city = document.getElementById("city").value;
+
+    // let icon = document.getElementById("iconSVG");
+    // icon.src = "../../weather/weatherDesign/assets/fewclouds-day.svg"
 }
